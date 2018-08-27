f9eefbd412f7cdd62a042f9b87c0048bd74e5ccd
Gwekkeo <gwendoline.cassand@gmail.com>
2018-08-27 15:49:15 +0200

    ajout page EDIT_USER

--- db/schema.rb
+++ db/schema.rb
@@ -10,8 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2018_08_27_104644) do
 
-ActiveRecord::Schema.define(version: 2018_08_27_102428) do
+  create_table "items", force: :cascade do |t|
+    t.string "title"
+    t.text "description"
+    t.decimal "price"
+    t.string "image_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,16 +32,6 @@ ActiveRecord::Schema.define(version: 2018_08_27_102428) do
     t.string "address"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-   end
-ActiveRecord::Schema.define(version: 2018_08_27_104644) do
-
-  create_table "items", force: :cascade do |t|
-    t.string "title"
-    t.text "description"
-    t.decimal "price"
-    t.string "image_url"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
 end
