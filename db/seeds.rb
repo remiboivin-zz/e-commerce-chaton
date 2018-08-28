1c09e21076d4245bb03a4cb1c630df955dd08bbc
Joanne <jonane_k3ly@hotmail.fr>
2018-08-28 12:20:46 +0200

    front sans slider

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,12 +6,12 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 require 'faker'
-20.times do
+20.times do |i|
   Item.create(
     title: Faker::Cat.name,
     description: Faker::Cat.breed,
     price: Faker::Stripe.ccv,
-    image_url: Faker::Avatar.image
+    image_url: "chaton#{i}.jpg"
   )
 
 end
