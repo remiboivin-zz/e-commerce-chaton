51249a5a252636c7d439b270a4c60d1ab9e15fe9
Mai Lan Peng <mailan.peng@gmail.com>
2018-08-28 14:40:14 +0200

    validation item

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,6 @@
 class Item < ApplicationRecord
-	has_and_belongs_to_many :cards
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :price, presence: true
+  validates :image_url, uniqueness: true
 end
