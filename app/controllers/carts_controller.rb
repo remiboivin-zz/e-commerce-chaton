fb7eabf7a196d375e215099c9aecdec05e9cb932
nanacy <nancy.cassand@gmail.com>
2018-08-29 15:08:50 +0200

    création du panier sur le retour de la page d'accueil si la personne est connecter

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -25,7 +25,6 @@ class CartsController < ApplicationController
   def show
     if user_signed_in?
       @id_user = current_user.id
-      mon_cart = Cart.create(user_id: @id_user)
       @cart_id = Cart.find_by(user_id: @id_user).id
 
     	@name_user = current_user.email
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,11 @@
 class StaticPagesController < ApplicationController
+  
   def home
   	@all = Item.all
+
+  	if user_signed_in?
+  		mon_cart = Cart.create(user_id: current_user.id)
+  	end
   end
+
 end
