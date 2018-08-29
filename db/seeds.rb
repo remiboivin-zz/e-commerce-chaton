b9f5df2b12dd0977caef1542e8614b2dbc943a47
Joanne <jonane_k3ly@hotmail.fr>
2018-08-29 10:48:45 +0200

    pull du mercredi 2

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 nb_de_user = 3
-nb_d_item = 4
+nb_d_item = 20
 nb_de_cart = 5
 num_cart_a_remplir = 4
 
@@ -8,7 +8,7 @@ require 'faker'
 
 nb_d_item.times do |i|
   Item.create(
-    title: Faker::Cat.name,
+    title: Faker::Dog.breed,
     description: Faker::Movie.quote,
     price: Faker::Number.decimal(2),
     image_url: "chaton#{i}.jpg"
@@ -43,27 +43,27 @@ end
 puts
 
 
-# # ---------------------------------------------------
-# # => ----        REMPLISSAGE DU CART CHOISI
-# # ---------------------------------------------------
+# # # ---------------------------------------------------
+# # # => ----        REMPLISSAGE DU CART CHOISI
+# # # ---------------------------------------------------
 
-puts
-puts "---- REMPLISSAGE DU CART #{num_cart_a_remplir} ----"
-2.times do
-  x = rand(1..Item.last.id)
-  begin
-    Store.create(item_id: x, cart_id: num_cart_a_remplir)
-    puts "Ajout de l'item N°#{x} dans le Cart N°#{num_cart_a_remplir}"
-  rescue Exception => e
-    puts "error :)\n Le cart #{num_cart_a_remplir} n'existe pas!!!"
-    break
-  end
-end
-puts "---- AFFICHAGE DU CART #{num_cart_a_remplir} ----"
-begin
-  Store.where(cart_id: num_cart_a_remplir).each do |i|
-    print i, " -> ", i.item_id, "\n"
-  end
-rescue Exception => e
-  puts "Le Cart n'existe pas :)"
-end
+# puts
+# puts "---- REMPLISSAGE DU CART #{num_cart_a_remplir} ----"
+# 2.times do
+#   x = rand(1..Item.last.id)
+#   begin
+#     Store.create(item_id: x, cart_id: num_cart_a_remplir)
+#     puts "Ajout de l'item N°#{x} dans le Cart N°#{num_cart_a_remplir}"
+#   rescue Exception => e
+#     puts "error :)\n Le cart #{num_cart_a_remplir} n'existe pas!!!"
+#     break
+#   end
+# end
+# puts "---- AFFICHAGE DU CART #{num_cart_a_remplir} ----"
+# begin
+#   Store.where(cart_id: num_cart_a_remplir).each do |i|
+#     print i, " -> ", i.item_id, "\n"
+#   end
+# rescue Exception => e
+#   puts "Le Cart n'existe pas :)"
+# end
