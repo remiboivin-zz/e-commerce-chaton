a5288e0ae1afc6fde93e30bc4902ccdfe40112d4
nanacy <nancy.cassand@gmail.com>
2018-08-29 10:53:02 +0200

    ajout

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,8 +4,7 @@ class Item < ApplicationRecord
   validates :price, presence: true
   validates :image_url, uniqueness: true
 
-	has_and_belongs_to_many :carts
-
+	# has_and_belongs_to_many :carts
 
 	has_many :stores
 	has_many :carts, through: :stores
