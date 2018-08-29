f0fb51fde81afea9f4b2f0111a717dc6ef1bb3b5
Joanne <jonane_k3ly@hotmail.fr>
2018-08-29 16:45:54 +0200

    front, ajout view article et boutons ajout panier

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,8 +1,30 @@
 class ItemsController < ApplicationController
+  # before_action :set_item, only: [:show]
+
   def index
-
   end
+
   def show
-
+  	@id_item_detail = Item.find(params[:id_item_detail])
+  	@id_to_send = params[:id_item_detail]
+  	@item = Item.find(params[:id_item_detail])
   end
+
+  def add
+  	@id_item = params[:id_item_to_add].to_i
+  	@item = Item.find(@id_item)
+
+  	mon_ajout = Store.new(cart_id: Cart.find_by(user_id: current_user.id).id, item_id: @item.id)
+  	mon_ajout.save
+
+  	# redirect_to "/item/#{@id_item}"
+  	redirect_to "/"
+  end
+
+# private
+
+# 	def set_item
+# 		@item = Item.find(params[:id])
+# 	end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,11 +4,11 @@ Rails.application.routes.draw do
   
   get '/carts/new', to: 'carts#new'
   
-  get '/carts/show', to: 'carts#show'
   get '/carts/edit/:id_item_to_del', to: 'carts#edit'
+  get '/carts/show/', to: 'carts#show'
 
-
-  get 'items/show'
+  get '/item/:id_item_detail', to: 'items#show'
+  get 'item/add/:id_item_to_add', to: 'items#add'
 
   devise_for :users
 end
