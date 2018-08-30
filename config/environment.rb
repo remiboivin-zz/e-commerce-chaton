6b0a4215a35301664107cdedc380c7f78c32ebdb
Mai Lan Peng <mailan.peng@gmail.com>
2018-08-30 13:49:34 +0200

    debug stripe initializer error

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,5 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+Stripe.api_key = Rails.configuration.stripe[:secret_key]
--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,9 @@ Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'static_pages#home'
   get '/profil', to: 'static_pages#profil'
-  
+
   get '/carts/new', to: 'carts#new'
-  
+
   get '/carts/edit/:id_item_to_del', to: 'carts#edit'
   get '/carts/show/', to: 'carts#show'
   get '/carts/pay/:money', to: 'carts#pay'
@@ -14,4 +14,5 @@ Rails.application.routes.draw do
   get '/item/add/:id_item_to_add', to: 'items#add'
 
   devise_for :users
+  resources :charges
 end
