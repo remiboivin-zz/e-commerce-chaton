02575648d98794f7d887485ae152018931e31980
Joanne <jonane_k3ly@hotmail.fr>
2018-08-30 11:31:10 +0200

    ajout du statut ADMIN et du dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root 'static_pages#home'
   get '/profil', to: 'static_pages#profil'
   
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_29_163232) do
+ActiveRecord::Schema.define(version: 2018_08_30_091419) do
 
   create_table "carts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -60,6 +60,7 @@ ActiveRecord::Schema.define(version: 2018_08_29_163232) do
     t.datetime "updated_at", null: false
     t.string "address"
     t.integer "cart_id"
+    t.boolean "admin"
     t.index ["cart_id"], name: "index_users_on_cart_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
