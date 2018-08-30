abd232fa5c58e836b74f9b85237fe0122cb8814a
Rémi Boivin <remi.boivin@epitech.eu>
2018-08-30 17:08:38 +0200

    Revert "add mailler for pay"
    This reverts commit 1c43d09e2b5bb9aed274d059d3f89b1ca3f636ca.

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -2,60 +2,31 @@ require 'mailjet'
 
 class UserMailer < ApplicationMailer
 
-  def contact
-    Mailjet.configure do |config|
-      config.api_key = '3c6830e7960ed7aa8e590777da567b5b'
-      config.secret_key = 'd289d0234577fba0373903cd457bb254'
-      config.default_from = 'remi50avj.c@live.fr'
-      config.api_version = "v3.1"
-    end
-  #@user = user
-  variable = Mailjet::Send.create(messages: [{
-      'From'=> {
-          'Email'=> 'remi50avJ.C@live.fr',
-          'Name'=> 'ChatonPix'
-      },
-      'To'=> [
-          {
-              'Email'=> 'remi.boivin@epitech.eu',
-              'Name'=> 'User'
-          }
-      ],
-      'Subject'=> 'commande!',
-      'TextPart'=> 'yes une commande !',
-      'HTMLPart'=> '<h3>Admin</h3><br />Yess une commande !'
-  }]
-  )
-p variable.attributes['Messages']
+    def contact
+      Mailjet.configure do |config|
+        config.api_key = '3c6830e7960ed7aa8e590777da567b5b'
+        config.secret_key = 'd289d0234577fba0373903cd457bb254'
+        config.default_from = 'remi50avj.c@live.fr'
+        config.api_version = "v3.1"
+      end
+    #@user = user
+    variable = Mailjet::Send.create(messages: [{
+        'From'=> {
+            'Email'=> 'remi50avJ.C@live.fr',
+            'Name'=> 'Mailjet Pilot'
+        },
+        'To'=> [
+            {
+                'Email'=> 'remi.boivin@epitech.eu',
+                'Name'=> 'test'
+            }
+        ],
+        'Subject'=> 'Your email flight plan!',
+        'TextPart'=> 'Dear , welcome to Mailjet! May the delivery force be with you!',
+        'HTMLPart'=> '<h3>Dear, welcome to Mailjet!</h3><br />May the delivery force be with you!'
+    }]
+    )
+  p variable.attributes['Messages']
 
-end
-
-def contact_admin(user)
-  Mailjet.configure do |config|
-    config.api_key = '3c6830e7960ed7aa8e590777da567b5b'
-    config.secret_key = 'd289d0234577fba0373903cd457bb254'
-    config.default_from = 'remi50avj.c@live.fr'
-    config.api_version = "v3.1"
   end
-@user = user
-variable = Mailjet::Send.create(messages: [{
-    'From'=> {
-        'Email'=> 'remi50avJ.C@live.fr',
-        'Name'=> 'ChatonPix'
-    },
-    'To'=> [
-        {
-            'Email'=> @user.email,
-            'Name'=> 'Admin'
-        }
-    ],
-    'Subject'=> 'commande!',
-    'TextPart'=> 'yes une commande !',
-    'HTMLPart'=> '<h3>Admin</h3><br />Yess une commande !'
-}]
-)
-p variable.attributes['Messages']
-
-end
-
   end
